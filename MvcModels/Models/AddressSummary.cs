a813c8b56ca296c7d068235c50675575c97ea9a8
Brian Barrow <briancbarrow@gmail.com>
2017-09-01 14:16:58 -0600

    Selectively Binding Properties

--- MvcModels/Models/AddressSummary.cs
+++ MvcModels/Models/AddressSummary.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Web.Mvc;
 
 namespace MvcModels.Models
 {
+    [Bind(Include = "City")]
     public class AddressSummary
     {
         public string City { get; set; }
