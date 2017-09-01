15e94488394f3d6c9f9b78bc2ef86f50ed6c52fb
Brian Barrow <briancbarrow@gmail.com>
2017-09-01 15:44:52 -0600

    Registering a Model Binder with an Attribute

--- MvcModels/Models/AddressSummary.cs
+++ MvcModels/Models/AddressSummary.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
+using MvcModels.Infrastructure;
 
 namespace MvcModels.Models
 {
-    //[Bind(Include = "City")]
+    [ModelBinder(typeof(AddressSummaryBinder))]
     public class AddressSummary
     {
         public string City { get; set; }
